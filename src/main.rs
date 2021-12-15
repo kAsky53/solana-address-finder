7096b750280b87d40e977683a6217c3250747167
Keisuke Wantanabe <keisukew53@gmail.com>
2021-12-15 18:52:16 -0500

    first commit

---
+++ src/main.rs
@@ -0,0 +1,24 @@
+use regex::Regex;
+use solana_sdk::pubkey::Pubkey;
+use solana_sdk::signature::Keypair;
+
+fn main() {
+    // input regexp pattern here
+    let re = Regex::new(r"^(ring|rn|rg|rng)").unwrap();
+
+    // iterator
+    let mut iter = 0;
+
+    loop {
+        let keypair = Keypair::new();
+        let public = Pubkey::new(&keypair.to_bytes()[32..64]);
+        iter += 1;
+
+        if re.is_match(&format!("{}", public)) {
+            println!("Iteration: {}", iter);
+            println!("Raw secret {:?}", keypair.to_bytes());
+            println!("Secret: {:?}", keypair.secret());
+            println!("Public: {}", public);
+        }
+    }
+}
